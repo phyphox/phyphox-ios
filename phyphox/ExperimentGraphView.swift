2ca76e5bb69d36167b65f43861d707f0b5f71f1d
Jonas Gessner <support@j-gessner.de>
2016-01-16 19:43:58 +0100

    Cleanup

--- phyphox/ExperimentGraphView.swift
+++ phyphox/ExperimentGraphView.swift
@@ -12,21 +12,19 @@ let q = dispatch_queue_create("queue", DISPATCH_QUEUE_SERIAL)
 
 public class ExperimentGraphView: ExperimentViewModule<GraphViewDescriptor> {
     let graph: JGGraphView
-    let imgView: UIImageView
+//    let imgView: UIImageView
     
     typealias T = GraphViewDescriptor
     
     required public init(descriptor: GraphViewDescriptor) {
         graph = JGGraphView()
-//        graph.path = JGGraphDrawer.drawPath([[0.0, 100.0]], ys: [[0.0, 100.0]], maxX: 100.0, maxY: 100.0, size: CGSizeMake(100.0, 100.0))
-//      
-        imgView = UIImageView()
+
+//        imgView = UIImageView()
         
         super.init(descriptor: descriptor)
         
-        backgroundColor = UIColor.redColor()
         addSubview(graph)
-        addSubview(imgView)
+//        addSubview(imgView)
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "setNeedsUpdate", name: DataBufferReceivedNewValueNotification, object: descriptor.yInputBuffer)
     }
@@ -40,7 +38,6 @@ public class ExperimentGraphView: ExperimentViewModule<GraphViewDescriptor> {
     private var lastXRange: Double?
     private var lastYRange: Double?
     private lazy var path: UIBezierPath = UIBezierPath()
-//    private var lastImage: UIImage?
     
     override func update() {
         autoreleasepool({ () -> () in
@@ -96,17 +93,11 @@ public class ExperimentGraphView: ExperimentViewModule<GraphViewDescriptor> {
                     }
                     
                     self.path = JGGraphDrawer.drawPath(xValues, ys: yValues, minX: minX, maxX: maxX, minY: minY!, maxY: maxY!, count: count, size: self.graph.bounds.size, reusePath: self.path, lastIndex: self.lastCount)
-//
+                    
                     self.lastXRange = xRange
                     self.lastYRange = yRange
                     
                     self.lastCount = count
-//
-//                    self.path.lineWidth = 2.0
-//                    UIColor.blackColor().setStroke()
-//                    
-//                    let img = JGGraphDrawer.drawPathToImage(self.path, size: self.bounds.size)
-//                    
                     
                     dispatch_sync(dispatch_get_main_queue(), { () -> Void in
                         self.graph.path = self.path
@@ -128,18 +119,7 @@ public class ExperimentGraphView: ExperimentViewModule<GraphViewDescriptor> {
     public override func layoutSubviews() {
         super.layoutSubviews()
         graph.frame = self.bounds
-        imgView.frame = self.bounds
-//        if graph.path != nil {
-//
-//            let b = graph.path!.bounds
-//            
-//            let scaleX = graph.frame.size.width/b.size.width
-//            let scaleY = graph.frame.size.height/b.size.height
-//            
-//            graph.path!.applyTransform(CGAffineTransformMakeScale(scaleX, scaleY))
-//            
-//            graph.refreshPath()
-//        }
+//        imgView.frame = self.bounds
     }
 }
 
--- phyphox/JGGraphView.swift
+++ phyphox/JGGraphView.swift
@@ -13,114 +13,71 @@ This code is taken from the Crono iOS app (https://itunes.apple.com/app/id980940
 import UIKit
 
 final class JGGraphLayer: CALayer {
-//    weak var path: CGPathRef? {
-//        didSet {
-//            setNeedsDisplay()
-//        }
-//    }
-//    
-//    var c = 0
-//    
-//    override func drawInContext(ctx: CGContext) {
-//        if c > 100 {
-//            print("End counting")
-//            return
-//        }
-//        if path != nil {
-//            CGContextSetStrokeColorWithColor(ctx, UIColor.redColor().CGColor);
-//            CGContextBeginPath(ctx)
-//            CGContextAddPath(ctx, path)
-//            CGContextStrokePath(ctx)
-//        }
-//        
-//        c++
-//    }
+    var childLayer: CALayer? {
+        willSet {
+            if newValue != childLayer {
+                childLayer?.removeFromSuperlayer()
+            }
+        }
+        
+        didSet {
+            if childLayer != nil {
+                addSublayer(childLayer!)
+            }
+        }
+    }
+    
+    override func layoutSublayers() {
+        super.layoutSublayers()
+        childLayer?.frame = self.bounds
+    }
 }
 
 final class JGGraphView: UIView {
-    let graphLayer: JGGraphLayer
-    
-//    override class func layerClass() -> AnyClass {
-//        return JGGraphLayer.self
-    //    }
-    
-    var path: UIBezierPath? {
-        set {
-            drawer.path = newValue
-            graphLayer.setNeedsDisplay()
-        }
-        
-        get {
-            return drawer.path
-        }
-        //        didSet {
-        ////            if path == nil {
-        ////                (self.layer as! JGGraphLayer).path = nil
-        ////            }
-        ////            else {
-        ////                (self.layer as! JGGraphLayer).path = path!.CGPath
-        ////            }
-        //
-        //            setNeedsDisplay()
-        //        }
+    override class func layerClass() -> AnyClass {
+        return JGGraphLayer.self
     }
     
-    func refreshPath() {
-        graphLayer.setNeedsDisplay()
-//        if path != nil {
-//            (self.layer as! JGGraphLayer).path = path!.CGPath
-//        }
-    }
+    private let drawer: Drawer
     
-//    let graphLayer: CALayer
-    
-    class Drawer: NSObject {
+    private class Drawer: NSObject {
         weak var path: UIBezierPath?
         
         override func drawLayer(layer: CALayer, inContext ctx: CGContext) {
             if path != nil {
-                CGContextSetStrokeColorWithColor(ctx, UIColor.whiteColor().CGColor);
+                CGContextSetStrokeColorWithColor(ctx, UIColor.blackColor().CGColor);
                 CGContextAddPath(ctx, self.path!.CGPath)
                 CGContextStrokePath(ctx)
             }
         }
     }
     
-    let drawer: Drawer
-    
     override init(frame: CGRect) {
         drawer = Drawer()
         
-        graphLayer = JGGraphLayer()
+        let graphLayer = CALayer()
         graphLayer.backgroundColor = UIColor.clearColor().CGColor
         graphLayer.delegate = drawer
         graphLayer.drawsAsynchronously = true
         
         super.init(frame: frame)
         
-        layer.addSublayer(graphLayer)
-//
-//        let layer = (self.layer as! JGGraphLayer)
-        
-//        layer.strokeColor = UIColor.blackColor().CGColor
-//        
-//        layer.lineWidth = 1.0
-//        layer.lineJoin = kCALineJoinRound
-//        layer.lineCap = kCALineCapRound
-//        
-//        layer.fillColor = UIColor.clearColor().CGColor
-        
-        
-//        layer.fillRule = kCAFillRuleEvenOdd
-//        layer.fillMode = kCAFillModeRemoved
-        
-//
+        (layer as! JGGraphLayer).childLayer = graphLayer
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
+    var path: UIBezierPath? {
+        set {
+            drawer.path = newValue
+            refreshPath()
+        }
         
-        graphLayer.frame = self.layer.bounds
+        get {
+            return drawer.path
+        }
+    }
+    
+    func refreshPath() {
+        (self.layer as! JGGraphLayer).childLayer!.setNeedsDisplay()
     }
 
     @available(*, unavailable)
