6032ca3defbaf285d6f6c8af8130f1974ce32c8e
Sebastian Kuhlen <kuhlen@physik.rwth-aachen.de>
2016-10-09 20:27:20 +0200

    Changed ExperimentViewController from UICollectionView-based implementation to UIScrollView-based one There was some trouble with cells randomly disappearing when changing the UICollectionViews frame, like on rotation or when adding the remote access info bar. I was not able to fix it, but the UICollectionView seems a little overkill in any case as the experiment layout usually fits all elements on the screen and in the cases when they don't (mostly landscape orientation) we do not need to remove and re-add hidden views constantly as the list remains short. So after despairing for two days (and still not being sure if encountering a bug in the API), I switched to a simple UIView nested in a UIScrollView. This is much simpler and runs great. The only thing left to do might be moving the Keyboard-tracking code from CollectionViewController to a more central place as it is used here as well.

--- phyphox iOS/phyphox/ExperimentViewController.swift
+++ phyphox iOS/phyphox/ExperimentViewController.swift
@@ -2,72 +2,117 @@
 //  ExperimentViewController.swift
 //  phyphox
 //
-//  Created by Jonas Gessner on 08.01.16.
-//  Copyright © 2016 Jonas Gessner. All rights reserved.
-//  By Order of RWTH Aachen.
+//  Created by Sebastian Kuhlen on 09.10.16.
+//  Copyright © 2016 RWTH Aachen. All rights reserved.
 //
 
 import UIKit
-import GCDWebServers
 
-final class ExperimentViewController: CollectionViewController {
+final class ExperimentViewController: UIViewController {
     private let modules: [UIView]
+    
+    private let scrollView = UIScrollView()
+    private let linearView = UIView()
+    
+    @available(*, unavailable)
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        fatalError("Unavailable")
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Unavailable")
+    }
     
     init(modules: [UIView]) {
         self.modules = modules
-        super.init()
-    }
-    
-    override class var viewClass: CollectionContainerView.Type {
-        get {
-            return ExperimentView.self
-        }
-    }
-    
-    override class var customCells: [String: UICollectionViewCell.Type]? {
-        get {
-            return ["ModuleCell" : ExperimentViewModuleCollectionViewCell.self]
-        }
+        super.init(nibName: nil, bundle: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardFrameChanged(_:)), name: UIKeyboardWillChangeFrameNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardFrameChanged(_:)), name:UIKeyboardDidChangeFrameNotification, object: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        selfView.collectionView.backgroundColor = kBackgroundColor
+        for module in modules {
+            linearView.addSubview(module)
+        }
+        scrollView.addSubview(linearView)
+        scrollView.backgroundColor = kBackgroundColor
+        scrollView.contentInset = UIEdgeInsetsMake(10, 0, 0, 0)
+        self.view.addSubview(scrollView)
     }
     
-    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return modules.count
-    }
-    
-    override func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        let s = modules[indexPath.row].sizeThatFits(self.view.frame.size)
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         
-        return CGSizeMake(collectionView.frame.size.width, s.height)
+        scrollView.frame = view.bounds
+        
+        var contentSize = CGRectMake(0, 0, view.bounds.width, 0)
+        
+        for module in modules {
+            let s = module.sizeThatFits(view.bounds.size)
+            module.frame = CGRectMake((view.bounds.width-s.width)/2.0, contentSize.height, s.width, s.height)
+            contentSize = CGRectMake(0, 0, view.bounds.width, contentSize.height + s.height)
+        }
+        
+        linearView.frame = contentSize
+        scrollView.contentSize = contentSize.size
     }
     
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 10.0, left: 0.0, bottom: 0.0, right: 0.0)
+    //MARK: - Keyboard handler
+    
+    dynamic private func keyboardFrameChanged(notification: NSNotification) {
+        func UIViewAnimationOptionsFromUIViewAnimationCurve(curve: UIViewAnimationCurve) -> UIViewAnimationOptions  {
+            let testOptions = UInt(UIViewAnimationCurve.Linear.rawValue << 16);
+            
+            if (testOptions != UIViewAnimationOptions.CurveLinear.rawValue) {
+                NSLog("Unexpected implementation of UIViewAnimationOptionCurveLinear");
+            }
+            
+            return UIViewAnimationOptions(rawValue: UInt(curve.rawValue << 16));
+        }
+        
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey]!.doubleValue!
+        
+        let curve = UIViewAnimationCurve(rawValue: notification.userInfo![UIKeyboardAnimationCurveUserInfoKey]!.integerValue!)!
+        
+        var bottomInset: CGFloat = 0.0
+        
+        if !CGRectIsEmpty(keyboardFrame) {
+            bottomInset = view.frame.size.height-view.convertRect(keyboardFrame, fromView: nil).origin.y
+        }
+        
+        var contentInset = scrollView.contentInset
+        var scrollInset = scrollView.scrollIndicatorInsets
+        
+        contentInset.bottom = bottomInset
+        scrollInset.bottom = bottomInset
+        
+        UIView.animateWithDuration(duration, delay: 0.0, options: [UIViewAnimationOptions.BeginFromCurrentState, UIViewAnimationOptionsFromUIViewAnimationCurve(curve)], animations: { () -> Void in
+            self.scrollView.contentInset = contentInset
+            self.scrollView.scrollIndicatorInsets = scrollInset
+            }, completion: nil)
     }
     
-    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ModuleCell", forIndexPath: indexPath) as! ExperimentViewModuleCollectionViewCell
+    override class func initialize() {
+        super.initialize()
         
-        let module = modules[indexPath.row]
-        
-        cell.module = module
-        
-        (module as! ExperimentViewModuleProtocol).setNeedsUpdate()
-        (module as! ExperimentViewModuleProtocol).triggerUpdate()
-        
-        return cell
+        NSNotificationCenter.defaultCenter().addObserver(self.self, selector: #selector(keyboardFrameWillChange(_:)), name: UIKeyboardWillChangeFrameNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self.self, selector: #selector(keyboardFrameDidChange(_:)), name:UIKeyboardDidChangeFrameNotification, object: nil)
     }
     
-    override func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
-        return 5.0
+    dynamic private class func keyboardFrameWillChange(notification: NSNotification) {
+        keyboardFrame = notification.userInfo![UIKeyboardFrameEndUserInfoKey]!.CGRectValue;
+        if (CGRectIsEmpty(keyboardFrame)) {
+            keyboardFrame = notification.userInfo![UIKeyboardFrameBeginUserInfoKey]!.CGRectValue;
+        }
     }
+    
+    dynamic private class func keyboardFrameDidChange(notification: NSNotification) {
+        keyboardFrame = notification.userInfo![UIKeyboardFrameEndUserInfoKey]!.CGRectValue;
+    }
+
+    
 }
