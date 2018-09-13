04f57f0551ff234ed12dcfc3be87cc3990805828
Jonas Gessner <support@j-gessner.de>
2018-09-13 19:40:49 +0200

    Remove XMLDictionary

--- External/checkout.sh
+++ External/checkout.sh
@@ -1,7 +1,5 @@
 echo Refreshing GitHub Sources
 
-echo Not checking out XMLDictionary, because the local version contains changes.
-
 . checkoutLatestCommit.sh JanX2/JXLS JXLS
 
 . checkoutLatestRelease.sh JonasGessner/JGProgressHUD JGProgressHUD
--- phyphox-iOS/phyphox/phyphox-Bridging-Header.h
+++ phyphox-iOS/phyphox/phyphox-Bridging-Header.h
@@ -2,7 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "XMLDictionary.h"
 #import "UIAlertController+PTExtensions.h"
 #import "BEMCheckbox.h"
 #import <ZipZap/ZipZap.h>
