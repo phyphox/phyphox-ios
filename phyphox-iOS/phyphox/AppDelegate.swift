84b7737385fd700e1b2bf9c0f48275ec4c0a7ecf
GTripathee <tripathee.gaurav@gmail.com>
2023-03-02 16:27:34 +0100

    fix: make the app mode adjustable with system wide dark and light mode and also fiy the issue of rendering dark background in contribute section

--- phyphox-iOS/phyphox/AppDelegate.swift
+++ phyphox-iOS/phyphox/AppDelegate.swift
@@ -100,8 +100,9 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         SettingBundleHelper.registerDefaults()
-        NotificationCenter.default.post(name: Notification.Name(rawValue: DidBecomeActiveNotification), object: nil)
         SettingBundleHelper.setAppMode(window: window)
+        NotificationCenter.default.post(name: Notification.Name(rawValue: DidBecomeActiveNotification), object: nil)
+        
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
