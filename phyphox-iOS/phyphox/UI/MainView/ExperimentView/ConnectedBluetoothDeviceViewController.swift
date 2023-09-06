267400421cd109e3f400e9c9da0b6a2d3c430a10
Gaurav Tripathee <tripathee.gaurav@gmail.com>
2023-09-06 09:46:00 +0200

    Show battery info when connected with the bluetooth devices (#12)
    * feat: show connected device information while experiment loaded from the bluetooth device
    
    * fix showing battery level eeventhougth there is no battery service
    
    * refactor:  change the name dependecy  in bluetooth connected device
    
    * ui: fix the graph border color when experiment opened from connected bluetooth device
    
    * refactor: remove extra if condition in updating connected bluetooth device

--- phyphox-iOS/phyphox/UI/MainView/ExperimentView/ConnectedBluetoothDeviceViewController.swift
+++ phyphox-iOS/phyphox/UI/MainView/ExperimentView/ConnectedBluetoothDeviceViewController.swift
@@ -105,6 +105,7 @@ class ConnectedBleDeviceCell: UICollectionViewCell {
     func getSignal(rssi: Int) -> UIImage{
       
         if rssi > -35 {
+<<<<<<< HEAD
             return UIImage(named: "cellular_level_4")!
         } else if rssi > -55 {
             return UIImage(named: "cellular_level_3")!
