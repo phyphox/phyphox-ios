0a0b138e039c7f4e4b84b6fc71dedac2625bab60
GTripathee <tripathee.gaurav@gmail.com>
2025-12-08 12:51:02 +0100

    refact: handle errors and make buffer reusables on spectroscopy analyzer file.

--- phyphox-iOS/phyphox/Experiments/Data-Input/Camera/Analysers/SpectroscopyAnalyzer.swift
+++ phyphox-iOS/phyphox/Experiments/Data-Input/Camera/Analysers/SpectroscopyAnalyzer.swift
@@ -9,15 +9,26 @@
 @available(iOS 14.0, *)
 class SpectroscopyAnalyzer: AnalyzingModule {
     
-    var result: DataBuffer?
+
+    private enum Constants {
+        static let kernelFunctionName = "readLuminaceVal"
+        static let threadGroupWidth = 16
+        static let threadGroupHeight = 16
+    }
+    
+    var analysisResult: DataBuffer?
     var xAxis: DataBuffer?
     
     var analyzisPipelineState : MTLComputePipelineState?
-    var outputBuffer: MTLBuffer?
-    var latestResults = (0..<720).map { Double($0) }
+    var metalOutputBuffer: MTLBuffer?
+    private var latestResults: [Double] = []
+    private var latestxAxis: [Double] = []
+    
+    var selectedWidthForAnalysis  = 0
+    var selectedHeightForAnalysis  = 0
     
     init(result: DataBuffer?, xAxis: DataBuffer?) {
-        self.result = result
+        self.analysisResult = result
         self.xAxis = xAxis
     }
     
@@ -25,10 +36,10 @@ class SpectroscopyAnalyzer: AnalyzingModule {
         guard let metalDevice = AnalyzingModule.metalDevice else { return }
         let gpuFunctionLibrary = AnalyzingModule.gpuFunctionLibrary
         
-        guard let intensityFunction = gpuFunctionLibrary?.makeFunction(name: "readLuminaceVal") else { return }
+        guard let readLuminanceFunction = gpuFunctionLibrary?.makeFunction(name: Constants.kernelFunctionName) else { return }
         
         do {
-            analyzisPipelineState = try metalDevice.makeComputePipelineState(function: intensityFunction)
+            analyzisPipelineState = try metalDevice.makeComputePipelineState(function: readLuminanceFunction)
         } catch {
             print("Failed to create pipeline analysis state, error \(error)")
         }
@@ -37,115 +48,93 @@ class SpectroscopyAnalyzer: AnalyzingModule {
     
     override func doUpdate(metalCommandBuffer: any MTLCommandBuffer, cameraImageTextureY: any MTLTexture, cameraImageTextureCbCr: any MTLTexture) {
         
-        guard let analyzisEncoding = metalCommandBuffer.makeComputeCommandEncoder() else { return }
+        guard let computeEncoder = metalCommandBuffer.makeComputeCommandEncoder() else { return }
         
         guard let analysisPipelineState = self.analyzisPipelineState else {
-            print("Failed to create analysisPipelineState")
-            analyzisEncoding.endEncoding()
+            print("Failed to find pipeline state")
+            computeEncoder.endEncoding()
             return
         }
         
-        analyzisEncoding.setComputePipelineState(analysisPipelineState)
+        computeEncoder.setComputePipelineState(analysisPipelineState)
         
-        analyzeTexture(analyzeEncoding: analyzisEncoding, cameraImageTextureY: cameraImageTextureY, cameraImageTextureCbCr: cameraImageTextureCbCr)
+        analyzeTexture(computeEncoder: computeEncoder, cameraImageTextureY: cameraImageTextureY, cameraImageTextureCbCr: cameraImageTextureCbCr)
        
     }
     
-    func analyzeTexture(analyzeEncoding : MTLComputeCommandEncoder, cameraImageTextureY: MTLTexture, cameraImageTextureCbCr: MTLTexture){
+    func analyzeTexture(computeEncoder : MTLComputeCommandEncoder, cameraImageTextureY: MTLTexture, cameraImageTextureCbCr: MTLTexture){
         guard let metalDevice = AnalyzingModule.metalDevice else { return }
         
-        let height = cameraImageTextureY.height
-        let width = cameraImageTextureY.width
+        selectedWidthForAnalysis = Int(selectionState.x2 - selectionState.x1)
+        selectedHeightForAnalysis = Int(selectionState.y2 - selectionState.y1)
         
-        let resultWidth = getSelectedArea().width
+        // Ensure dimensions are valid to prevent crashes
+        guard selectedWidthForAnalysis > 0, selectedHeightForAnalysis > 0 else {
+            computeEncoder.endEncoding()
+            return
+        }
         
-        let calculatedThreadSize = calculateThreadSize(selectedWidth: getSelectedArea().width, selectedHeight: getSelectedArea().height)
-        
-        outputBuffer = metalDevice.makeBuffer(length: resultWidth * MemoryLayout<Float>.stride, options: .storageModeShared)
+        let requiredBytes = selectedWidthForAnalysis * MemoryLayout<Float>.stride
+        if metalOutputBuffer == nil || metalOutputBuffer!.length < requiredBytes {
+            metalOutputBuffer = metalDevice.makeBuffer(length: requiredBytes, options: .storageModeShared)
+        }
         
         let selectionBuffer = metalDevice.makeBuffer(bytes: &selectionState, length: MemoryLayout<SelectionState>.size, options: .storageModeShared)
         
-        analyzeEncoding.setTexture(cameraImageTextureY, index: 0)
-        analyzeEncoding.setTexture(cameraImageTextureCbCr, index: 1)
-        analyzeEncoding.setBuffer(outputBuffer, offset: 0, index: 0)
-        analyzeEncoding.setBuffer(selectionBuffer, offset: 0, index: 1)
+        computeEncoder.setTexture(cameraImageTextureY, index: 0)
+        computeEncoder.setTexture(cameraImageTextureCbCr, index: 1)
+        computeEncoder.setBuffer(metalOutputBuffer, offset: 0, index: 0)
+        computeEncoder.setBuffer(selectionBuffer, offset: 0, index: 1)
         
-        analyzeEncoding.dispatchThreadgroups(calculatedThreadSize.gridSize, threadsPerThreadgroup: calculatedThreadSize.threadGroupSize)
+        let gridSize = calculateGridSize(width: selectedWidthForAnalysis, height: selectedHeightForAnalysis)
         
-        analyzeEncoding.endEncoding()
-        
-    }
-    
-    
-    func analyze(analyzeEncoding : MTLComputeCommandEncoder,
-                 cameraImageTextureY: MTLTexture,
-                 cameraImageTextureCbCr: MTLTexture){
-        
-        guard let metalDevice = AnalyzingModule.metalDevice else { return }
-        
-        let regionWidth = getSelectedArea().width
-        let regionHeight = getSelectedArea().height
-        
-        
-        let bufferSize = 1 * regionHeight
-        
-        let calculateGridAndGroupSize = calculateThreadSize(selectedWidth: 1, selectedHeight: regionHeight)
-        
-        outputBuffer = metalDevice.makeBuffer(length: regionWidth * MemoryLayout<Float>.stride, options: .storageModeShared)
-        
-        let selectionBuffer = metalDevice.makeBuffer(bytes: &selectionState, length: MemoryLayout<SelectionState>.size, options: .storageModeShared)
-        
-        analyzeEncoding.setTexture(cameraImageTextureY, index: 0)
-        analyzeEncoding.setTexture(cameraImageTextureCbCr, index: 1)
-        analyzeEncoding.setBuffer(outputBuffer, offset: 0, index: 0)
-        analyzeEncoding.setBuffer(selectionBuffer, offset: 0, index: 1)
-        
-        analyzeEncoding.dispatchThreadgroups(calculateGridAndGroupSize.gridSize, threadsPerThreadgroup: calculateGridAndGroupSize.threadGroupSize)
-        
-        analyzeEncoding.endEncoding()
+        computeEncoder.dispatchThreadgroups(gridSize.threadgroups, threadsPerThreadgroup: gridSize.threadsPerThreadgroup)
+        computeEncoder.endEncoding()
         
     }
     
     override func prepareWriteToBuffers(cameraSettings: CameraSettingsModel) {
         
-        guard let baseAddress = outputBuffer?.contents() else {
-            print("Error: buffer.contents() returned nil")
-            return
+        guard let buffer = metalOutputBuffer, selectedWidthForAnalysis > 0 else { return }
+        
+        let luminancePointer = buffer.contents().bindMemory(to: Float.self, capacity: selectedWidthForAnalysis)
+        
+        if latestResults.count != selectedWidthForAnalysis {
+            latestResults = Array(repeating: 0.0, count: selectedWidthForAnalysis)
         }
         
-        let textureWidth = (outputBuffer?.length ?? 1) / MemoryLayout<Float>.stride
-        let luminancePtr = baseAddress.bindMemory(to: Float.self, capacity: textureWidth)
-        
-        latestResults =  (0..<getSelectedArea().width).map { Double($0) }
-        for i in 0..<latestResults.count {
-            latestResults[i] = Double(luminancePtr[i])
+        for i in 0..<selectedWidthForAnalysis {
+            latestResults[i] = Double(luminancePointer[i])
         }
     }
     
     override func writeToBuffers() {
         self.xAxis?.clear(reset: true)
-        self.result?.clear(reset: true)
+        self.analysisResult?.clear(reset: true)
         
+        guard selectedWidthForAnalysis > 0 else { return }
         
-        let xAxisValue = Array(0..<(((self.getSelectedArea().width ) - 1))).map{ Double($0) }
-        self.xAxis?.appendFromArray(xAxisValue)
-    
-        if let resultBuffer = result {
-            resultBuffer.appendFromArray(latestResults)
-           
-        }
+        if latestxAxis.count != (selectedWidthForAnalysis - 1) {
+            latestxAxis = (0..<(selectedWidthForAnalysis - 1)).map { Double($0) }
+                }
+        
+        self.xAxis?.appendFromArray(latestxAxis)
+        self.analysisResult?.appendFromArray(latestResults)
+
     }
     
-    func calculateThreadSize(selectedWidth: Int, selectedHeight: Int) -> (threadGroupSize: MTLSize, gridSize: MTLSize, numOfThreadGroups: Int) {
+    func calculateGridSize(width: Int, height: Int) -> (threadgroups: MTLSize, threadsPerThreadgroup: MTLSize) {
        
-        let threadGroupSize = MTLSize(width: 16, height: 16, depth: 1)
-         // Dispatch the compute shader with the size of the selected bounding box
-        let threadgroupsX = (selectedWidth + threadGroupSize.width - 1) / threadGroupSize.width;
-        let threadgroupsY = (selectedHeight + threadGroupSize.height - 1) / threadGroupSize.height;
-        let _gridSize = MTLSize(width: threadgroupsX, height: threadgroupsY, depth: 1)
-        let _numThreadSize = (_gridSize.width * _gridSize.height)
+        let threadsPerGroup = MTLSize(width: Constants.threadGroupWidth,
+                                              height: Constants.threadGroupHeight,
+                                              depth: 1)
         
-        return (threadGroupSize: threadGroupSize, gridSize: _gridSize, numOfThreadGroups: _numThreadSize )
+        let threadgroupsX = (width + threadsPerGroup.width - 1) / threadsPerGroup.width
+        let threadgroupsY = (height + threadsPerGroup.height - 1) / threadsPerGroup.height
+                
+        let threadgroups = MTLSize(width: threadgroupsX, height: threadgroupsY, depth: 1)
+                
+        return (threadgroups, threadsPerGroup)
          
     }
     
